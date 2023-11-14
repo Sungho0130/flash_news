e8f91d5799eea9730f682f96f75a4986670da06c
HSJ <ghdtmd4117@naver.com>
2023-11-14 09:46:05 +0900

    탬플릿 main작업 하는중 팀원과 틀을 맞추기 위한 커밋

--- NewS_page/views.py
+++ NewS_page/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from django.core.paginator import Paginator
 
-# Create your views here.
+def main_page(request):
+    return render(request, "main.html")
