1d5605792f7883c20ab93ed60c0286a71852104a
Choi Sungho <starhochoi@gmail.com>
2023-11-16 16:37:02 +0900

    코드 정리 및 주석 처리 gitignore 추가

--- NewS_page/urls.py
+++ NewS_page/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("", views.main_page, name="main_page"),
     path('your-model-endpoint/', views.your_model_endpoint, name='your-model-endpoint'),
     path("ns_page", views.ns_page, name="ns_page"),
-    path('/<str:category>/', views.detail_page, name='category_news'),
+    path('<str:category>/', views.detail_page, name='category_news'),
 ]
