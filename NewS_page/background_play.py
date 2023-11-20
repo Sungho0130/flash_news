600484670c0c32c6282dd8888dcfa7e6c3f38322
Kim sung jin <dolrea77@gmail.com>
2023-11-20 12:03:20 +0900

    중간 커밋 카테고리 크롤링 완료 메인 크롤링 코드 최적화 현재 카테고리페이지에서 쿼리값이 표시가 안됨 쿼리 값은 잘가지만 for문 사용시 보이지 않음

--- NewS_page/background_play.py
+++ NewS_page/background_play.py
@@ -1,12 +1,14 @@
+import sched
+
 from apscheduler.schedulers.background import BackgroundScheduler
 from .news_craw import newscrawring, category_crawring
 from .models import Crawring, Crawring_ct
 from .model_call import summary
-from datetime import datetime
+from datetime import datetime, timedelta
 from tqdm import tqdm
 
 def data_num():
-    max_records = 50
+    max_records = 40
     current_records = Crawring.objects.count()
 
     if current_records > max_records:
@@ -18,12 +20,12 @@ def data_num():
             record.delete()
 
 def data_num_ct(category):
-    max_records = 90
+    max_records = 60
     current_records = Crawring_ct.objects.filter(category=category).count()
 
     if current_records > max_records:
         # 최신 데이터 기준으로 오래된 레코드를 선택
-        records_to_delete = Crawring_ct.objects.filter(category=category).order_by('created_at')[:current_records - max_records]
+        records_to_delete = Crawring_ct.objects.filter(category=category).order_by('created_at_ct')[:current_records - max_records]
 
         # 선택된 레코드를 삭제
         for record in records_to_delete:
@@ -50,38 +52,45 @@ def job():
                 Crawring.objects.create(title=title, content=content, img=img, src=src)
 
         print('home 데이터베이스 저장 완료')
-        # 데이터 베이스 50개 초과시 오래된 것 부터 삭제
-        data_num()
-
-        if not Crawring.objects.filter(summarize=''):
-            print('비어있는 summarize 없음')
-
-        else:
-            for instance in tqdm(Crawring.objects.filter(summarize='')):
-                # 이미 채워진 content를 사용
-                content = instance.content
-
-                # summary 모델을 돌리기
-                summarize = summary(content)
-
-                # summarize가 비어있는 경우에만 저장
-                if not instance.summarize:
-                    instance.summarize = summarize
-                    instance.save()
-
-            print('요약 모델 작동 완료')
 
     else:
         print('크롤링 필요없음')
+
+    # 데이터 베이스 50개 초과시 오래된 것 부터 삭제
+    data_num()
+
+    if not Crawring.objects.filter(summarize=''):
+        print('비어있는 summarize 없음')
+
+    else:
+        for instance in tqdm(Crawring.objects.filter(summarize='')):
+            # 이미 채워진 content를 사용
+            content = instance.content
+
+            if len(content)>10:
+                # summary 모델을 돌리기
+                summarize = summary(content)
+            else:
+                summarize = content
+
+            # summarize가 비어있는 경우에만 저장
+            if not instance.summarize:
+                instance.summarize = summarize
+                instance.save()
+
+        print('요약 모델 작동 완료')
+
+
 
     cate_list = ['society', 'politics', 'economic', 'culture', 'entertain', 'sports', 'digital']
 
     for cate in cate_list:
 
         news_ct = category_crawring(cate)
+
         print(f'{cate} 크롤링 완료!')
 
-        if len(news_ct['title']) != 0:
+        if len(news_ct['title']) != None:
 
             for i in range(len(news_ct['title'])):
                 # 중복 확인
@@ -95,20 +104,29 @@ def job():
                     # Crawring 모델 인스턴스 생성 및 저장
                     Crawring_ct.objects.create(title_ct=title_ct, content_ct=content_ct, img_ct=img_ct, src_ct=src_ct, category=category)
 
-        print(f'{cate} 데이터베이스 저장 완료')
+            print(f'{cate} 데이터베이스 저장 완료')
+
+        else:
+            print('크롤링 필요없음')
+
 
         # 해당카테고리의 오래된 순서로 90개초과시 삭제
         data_num_ct(cate)
+
         if not Crawring_ct.objects.filter(summarize_ct=''):
             print('비어있는 summarize_ct가 없습니다')
 
         else:
             for instance in tqdm(Crawring_ct.objects.filter(summarize_ct='')):
+
                 # 이미 채워진 content를 사용
                 content_ct = instance.content_ct
-
-                # summary 모델을 돌리기
-                summarize_ct = summary(content_ct)
+                print('src: ', instance.src_ct)
+                if len(content_ct)>10:
+                    # summary 모델을 돌리기
+                    summarize_ct = summary(content_ct)
+                else:
+                    summarize_ct = content_ct
 
                 # summarize가 비어있는 경우에만 저장
                 if not instance.summarize_ct:
@@ -117,9 +135,12 @@ def job():
 
             print('요약 모델 작동 완료')
 
+
 def main():
     sched = BackgroundScheduler()
-    sched.add_job(job,'interval', minutes=30, id='test', next_run_time=datetime.now())
+
+
+    sched.add_job(job,'interval', minutes=60, id='test') #, next_run_time=datetime.now()
     sched.start()
 
 if __name__ == '__main__':
