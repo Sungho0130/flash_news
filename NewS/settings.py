89ab1595be0df4273ea6428c8c20983476e4390e
dolrea77 <dolrea77@gmail.com>
2023-11-24 15:34:54 +0900

    멀티쓰레드true

--- NewS/settings.py
+++ NewS/settings.py
@@ -39,7 +39,7 @@ SECRET_KEY = get_secret("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", "192.168.108.9"]
 
 
 # Application definition
