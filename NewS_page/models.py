485d5c08696a9349de8e7e96ba1883c4e3632684
dolrea77 <dolrea77@gmail.com>
2023-12-02 19:37:48 +0900

    크롤링 코드 합본 newscrawring, newstrawring_ct 함수 한번에 동작하도록 변경 후 모델을 Crawring_ct 하나로 만 쓰도록 모델 변경, view도 변경 참고로 크롤링시 메인페이지 크롤링시는 category가 main으로 저장 뷰에서 네브바 눌리면 get으로 받는 카테고리값이 홈키는 없기때문에 category값이 없다면 main으로 반환하도록 get을 사용

--- NewS_page/models.py
+++ NewS_page/models.py
@@ -1,12 +1,6 @@
 from django.db import models
 # Create your models here.
-class Crawring(models.Model):
-    title = models.CharField(max_length=100, unique=True, primary_key=True)
-    content = models.TextField()
-    img = models.CharField(max_length=100)
-    src = models.CharField(max_length=100)
-    summarize = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
+
 class Crawring_ct(models.Model):
     title_ct = models.CharField(max_length=100, unique=True, primary_key=True)
     content_ct = models.TextField()
--- NewS_page/views.py
+++ NewS_page/views.py
@@ -4,39 +4,26 @@ from django.core.paginator import Paginator
 from django.core.mail import EmailMessage
 import time
 def main_page(request):
-    # 요약이 없는 모든 Crawring 객체를 가져오고 생성 날짜 순으로 정렬
-    craw = Crawring.objects.exclude(summarize='').order_by('-created_at')
 
-    # 요청에서 카테고리 매개변수가 제공되었는지 확인
-    if request.GET.get('category', None):
-        cate = request.GET.get('category', None)
-        print('cate: ', cate)
 
-        # 카테고리별로 필터링된 Crawring_ct 객체를 가져오고 요약이 없는 것은 제외
-        craw_ct = Crawring_ct.objects.filter(category=cate).exclude(summarize_ct='').order_by('-created_at_ct')
+    cate = request.GET.get('category', "main")
 
-        # 필터링된 쿼리셋으로 Paginator를 초기화하고 페이지당 항목 수를 설정
-        paginator = Paginator(craw_ct, 10)
+    print('cate: ', cate)
 
-        # 요청에서 현재 페이지 번호를 가져옴
-        page_number = request.GET.get('page')
+    # 카테고리별로 필터링된 Crawring_ct 객체를 가져오고 요약이 없는 것은 제외
+    craw_ct = Crawring_ct.objects.filter(category=cate).exclude(summarize_ct='').order_by('-created_at_ct')
 
-        # Paginator에서 해당 페이지를 가져옴
-        page = paginator.get_page(page_number)
+    # 필터링된 쿼리셋으로 Paginator를 초기화하고 페이지당 항목 수를 설정
+    paginator = Paginator(craw_ct, 10)
 
-        return render(request, "main.html", {'craw_ct': page, 'category': cate})
-    else:
+    # 요청에서 현재 페이지 번호를 가져옴
+    page_number = request.GET.get('page')
 
-        # 메인 Crawring 쿼리셋용 Paginator를 초기화
-        paginator = Paginator(craw, 10)  # 필요에 따라 숫자를 변경
+    # Paginator에서 해당 페이지를 가져옴
+    page = paginator.get_page(page_number)
 
-        # 요청에서 현재 페이지 번호를 가져옴
-        page_number = request.GET.get('page')
+    return render(request, "main.html", {'craw_ct': page, 'category': cate})
 
-        # Paginator에서 해당 페이지를 가져옴
-        page = paginator.get_page(page_number)
-
-        return render(request, "main.html", {'craw': page})
 
 def email_page(request):
     if request.method == 'POST':
